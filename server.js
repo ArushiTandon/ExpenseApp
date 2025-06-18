9fa318a0fef9879f02209d335e6cdaeaec155b88
Arushi Tandon <arushitandon007@gmail.com>
2025-06-18 17:10:26 +0530

    Refactor server.js: Remove unused dependencies and logging middleware

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const connectDB = require('./util/db');
 const passport = require('./middlewares/auth');
-const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
@@ -12,9 +10,6 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use(passport.initialize());
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -29,7 +24,6 @@ app.get('/user', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Routes
 app.use('/Cashfreeservices', express.static(path.join(__dirname, 'Cashfreeservices')));
 app.use('/user', require('./routes/userRoutes'));
 app.use('/expense', require('./routes/expenseRoutes'));
